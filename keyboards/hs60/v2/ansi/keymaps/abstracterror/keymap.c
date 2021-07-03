6ded0d021266fad164c2c27bba9b4a33b2fc8b61
abstracterror <drew@abstracterror.com>
2021-07-03 12:10:49 +0100

    Use mod-taps to put shift on homing keys
    Hold F for left shift, and J for right shift.

--- keyboards/hs60/v2/ansi/keymaps/abstracterror/keymap.c
+++ keyboards/hs60/v2/ansi/keymaps/abstracterror/keymap.c
@@ -18,6 +18,8 @@
 // Helpful defines
 #define HYPCAPS MT(MOD_HYPR, KC_CAPS)
 #define SPC_FN1 LT(1, KC_SPACE)
+#define LS_F MT(MOD_LSFT, KC_F)
+#define RS_J MT(MOD_RSFT, KC_J)
 
 // Layer names
 #define _DF 0
@@ -30,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_DF] = LAYOUT_60_ansi(
     KC_GESC,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
-    HYPCAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
+    HYPCAPS,  KC_A,     KC_S,     KC_D,     LS_F,     KC_G,     KC_H,     RS_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,
     KC_LCTL,  KC_LALT,  KC_LGUI,                                SPC_FN1,                                KC_RGUI,  KC_RALT,  MO(_FN),  KC_RCTL),
 
