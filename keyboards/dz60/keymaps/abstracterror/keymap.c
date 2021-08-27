c4aaa68ef0d9d5a61c38b54facdd96a904e3ccee
abstracterror <drew@abstracterror.com>
2021-08-27 11:23:58 +0100

    Add symbol layer tap on middle space
    Also puts 0 on right space in number layer.

--- keyboards/dz60/keymaps/abstracterror/keymap.c
+++ keyboards/dz60/keymaps/abstracterror/keymap.c
@@ -24,6 +24,7 @@
 #define SY_SPC LT(_SY, KC_SPC)
 #define NM_BSPC LT(_NM, KC_BSPC)
 #define SY_BSPC LT(_SY, KC_BSPC)
+#define SY_ENT LT(_SY, KC_ENT)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -32,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         KC_CAPS,          LCTL_A,  LALT_S,  LGUI_D,  LSFT_F,  KC_G,    KC_H,    RSFT_J,  RGUI_K,  RALT_L,  RCTL_SC, KC_QUOT,          KC_ENT,
         KC_LSFT, XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          XXXXXXX,
-        KC_LCTL, KC_LALT, KC_LGUI,                   NM_BSPC, KC_ENT,  FN_SPC,                    KC_RGUI, KC_RALT, XXXXXXX, MO(_CT), KC_RCTL
+        KC_LCTL, KC_LALT, KC_LGUI,                   NM_BSPC, SY_ENT,  FN_SPC,                    KC_RGUI, KC_RALT, XXXXXXX, MO(_CT), KC_RCTL
     ),
 
     [_FN] = LAYOUT(
@@ -40,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,          _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______, _______, _______,
         _______,          KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______,
         _______, XXXXXXX, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_INS,  _______,          XXXXXXX,
-        _______, _______, _______,                   SY_BSPC, _______, _______,                   _______, _______, XXXXXXX, _______, _______
+        _______, _______, _______,                   _______, _______, _______,                   _______, _______, XXXXXXX, _______, _______
     ),
 
     [_NM] = LAYOUT(
@@ -48,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,          KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, _______, _______,
         _______,          KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, _______, KC_EQL,  KC_4,    KC_5,    KC_6,    KC_MINS, _______,          _______,
         _______, XXXXXXX, _______, _______, _______, _______, _______, _______, KC_1,    KC_2,    KC_3,    KC_BSLS, _______,          XXXXXXX,
-        _______, _______, _______,                   _______, _______, SY_SPC,                    _______, _______, XXXXXXX, _______, _______
+        _______, _______, _______,                   _______, _______, KC_0,                      _______, _______, XXXXXXX, _______, _______
     ),
 
     [_SY] = LAYOUT(
