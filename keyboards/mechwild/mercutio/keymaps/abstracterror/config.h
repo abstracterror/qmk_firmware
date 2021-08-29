f4c7722373b89c9a5bb2387169364db275997690
abstracterror <drew@abstracterror.com>
2021-08-29 18:01:09 +0100

    Disable retro tapping
    This caused spurious presses of the mod tap keys when attempting to use
    modifiers with mouse clicks.

--- keyboards/mechwild/mercutio/keymaps/abstracterror/config.h
+++ keyboards/mechwild/mercutio/keymaps/abstracterror/config.h
@@ -29,8 +29,3 @@
 // This allows use as a modifier immediately after a tap.
 // See `get_tapping_force_hold` in keymap.c.
 #define TAPPING_FORCE_HOLD_PER_KEY
-
-// Treat hold and release without pressing another key as a tap. This prevents
-// dropped letters when a key is accidentally held for longer than the tapping
-// term.
-#define RETRO_TAPPING
