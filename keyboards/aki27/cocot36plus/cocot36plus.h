5ca2c8a7be469b669a08aaabc050874e98b6ad6c
abstracterror <drew@abstracterror.com>
2025-03-02 16:49:21 +0000

    Update aki27/cocot36plus from designer's repo
    Updates the keyboard definition for aki27/cocot36plus from the
    designer's Vial repository, https://github.com/aki27kbd/vial-qmk. In
    particular, this includes their changes to improve the smoothness of the
    trackball.

--- keyboards/aki27/cocot36plus/cocot36plus.h
+++ keyboards/aki27/cocot36plus/cocot36plus.h
@@ -18,16 +18,15 @@
 
 #include "quantum.h"
 
-
 typedef union {
-    uint32_t raw;
+    uint64_t raw;
     struct {
         uint8_t cpi_idx;
         uint8_t scrl_div;
         uint8_t rotation_angle;
-        int8_t scrl_inv;
-        bool scrl_mode;
         bool auto_mouse;
+        bool scrl_inv;
+        bool scrl_mode;
         report_mouse_t last_mouse;
     };
 } cocot_config_t;
@@ -35,7 +34,9 @@ typedef union {
 extern cocot_config_t cocot_config;
 
 enum cocot_keycodes {
-    CPI_SW = QK_KB,
+
+    COCOT_SAFE_RANGE = SAFE_RANGE,
+    CPI_SW,
     SCRL_SW,
     ROT_R15,
     ROT_L15,
@@ -44,6 +45,16 @@ enum cocot_keycodes {
     SCRL_IN,
     AM_TOG
 };
+
+#define CPI_SW QK_KB_0
+#define SCRL_SW QK_KB_1
+#define ROT_R15 QK_KB_2
+#define ROT_L15 QK_KB_3
+#define SCRL_MO QK_KB_4
+#define SCRL_TO QK_KB_5
+#define SCRL_IN QK_KB_6
+#define AM_TOG QK_KB_7
+
 
 bool encoder_update_user(uint8_t index, bool clockwise);
 bool encoder_update_kb(uint8_t index, bool clockwise);
--- keyboards/aki27/cocot36plus/readme.md
+++ keyboards/aki27/cocot36plus/readme.md
@@ -1,6 +1,6 @@
-# cocot38mini
+# cocot36plus
 
-![cocot38mini](imgur.com image replace me!)
+![cocot36plus](imgur.com image replace me!)
 
 *A short description of the keyboard/project*
 
@@ -10,11 +10,11 @@
 
 Make example for this keyboard (after setting up your build environment):
 
-    make cocot36plus:default
+    make cocot36plus/v2:vial
 
 Flashing example for this keyboard:
 
-    make cocot38mini:default:flash
+    make cocot36plus/vial:flash
 
 See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
 
